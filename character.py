b7c0e06c5be5e5a9dc26dc772ba9a465262b6d74
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-02-28 21:08:08 +0000

    game v2

--- character.py
+++ character.py
@@ -1,21 +1,56 @@
-                                                                # This video was used as a tutorial for me to understand Object orientated programming using python. 
+                                                                # This video was used as a tutorial for me to better understand Object orientated programming using python. 
                                                                 # https://www.youtube.com/watch?v=cM_ocyOrs_k
-                                                                # This is used for educational purposes only.
 
 
 
 from weapon import fists
+from health_bar import HealthBar
 
                                                                 # Assigning the values of name, health and damage to the object itself using the self method.
 class Character:
-                                                                #race = "Human" class level variable - shared across all instances of the object
-    def __init__(self, name: str, health: int) -> None:
-        self.name = name                                        # object level variables - they will be unique to the created objects
+                                                                #race = "Human" class level variable - shared across all instances of the object.
+    def __init__(self, 
+                 name: str, 
+                 health: int) -> None:
+        self.name = name                                        # object level variables - they will be unique to the created objects.
         self.health = health
         self.health_max = health
         
-        self.weapon = fists                                     # All characters will now have fists as a weapon
+        self.weapon = fists                                     # All characters will now have fists as a weapon.
         
     def attack(self, target) -> None:                           # this will only take one object in this case the target we want to attack
-        target.health -= self.weapon.damage                            # this will reduce the health of target by the damage of the attacker(object)
+        target.health -= self.weapon.damage                     # this will reduce the health of target by the damage of the attacker(object)
         target.health = max(target.health, 0)                   # this avoids going below zero
+        target.health_bar.update()
+        print(f"{self.name} dealt {self.weapon.damage} damage to "
+              f"{target.name} with {self.weapon.name}")
+        
+        
+class Hero(Character):                                          # refering to the parent class will inherit from character class/
+    def __init__(self, 
+                 name: str, 
+                 health: int) -> None:
+        super().__init__(name=name, health=health)
+        
+        self.default_weapon = self.weapon
+        
+        self.health_bar = HealthBar(self, color="green")
+        
+    def equip(self, weapon) -> None:
+        self.weapon = weapon                                    # this method overwrites the current weapon for the hero with a new one.
+        print(f"{self.name} equipped a(n) {self.weapon.name}!")
+        
+    def drop(self):
+        print(f"{self.name} dropped the {self.weapon.name}!")
+        self.weapon = self.default_weapon                       # this method allows the hero to drop a weapon.
+        
+class Enemy(Character):                                         # refering to the parent class will inherit from character class.
+    def __init__(self, 
+                 name: str, 
+                 health: int,
+                 weapon,) -> None:
+        super().__init__(name=name, health=health)
+        
+        self.weapon = weapon
+        
+        self.health_bar = HealthBar(self, color="red")
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from character import Character                                       # importing the character object from character file
+import os
+from character import Hero, Enemy                                    # importing the Hero and Enemy object from character file
+from weapon import short_bow, iron_sword, fists
 
-hero = Character(name="Hero", health=100)
-enemy = Character(name="Enemy", health=100)
+hero = Hero(name="Hero", health=100)
+hero.equip(iron_sword)
+enemy = Enemy(name="Enemy", health=100, weapon=short_bow)
 
 while True:                                                          # invoking the attack method for hero and enemy.
+    os.system("clear")
     hero.attack(enemy)
     enemy.attack(hero)
     
-    print(f"Health of Hero {hero.health} and Health of Enemy {enemy.health} ")
+    hero.health_bar.draw()
+    enemy.health_bar.draw()
     
+    if hero.weapon != fists:
+        hero.drop()
     input()
